424c3ddcc15d3e8586c52ba40ae900768e4218bb
amanhnb88 <aldry.manu02@gmail.com>
2025-10-25 01:55:34 +0900

    Create build.gradle.kts

---
+++ Moviebox/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.hexated.moviebox"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("com.github.recloudstream:cloudstream:5.0.0-beta.2")
+}
